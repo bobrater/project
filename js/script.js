fa98dd7b3a724b731281a811a84cca7c81793689
boris <boriko13@gmail.com>
2022-03-05 12:46:22 +0300

    Добавил проходи садись в alert

--- js/script.js
+++ js/script.js
@@ -9,5 +9,16 @@ const user = 'Boris';
 alert(`Привет, ${user}`);
 
 
+let incr = 10,
+    decr = 10;
 
+//incr++;
+//decr--;
+console.log(++incr);
+console.log(--decr);
+console.log(2+2*4 == 8);
+
+const isChecked = true,
+    isClose = true;
+console.log(isChecked && !isClose)
 
